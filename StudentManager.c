6e1c93bdaafb2ec3412b72ed1398fb7ac8a24991
zhongdh7 <zhongdh7@mail2.sysu.edu.cn>
2026-09-26 23:26:46 +0800

    初始化学生管理者相关的结构

---
+++ StudentManager.c
@@ -0,0 +1,6 @@
+#include "StudentManager.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ForwardList.h"
+#include "Student.h"
---
+++ StudentManager.h
@@ -0,0 +1,13 @@
+#ifndef STUDENT_MANAGER_H
+#define STUDENT_MANAGER_H
+#include "ForwardList.h"
+#include "Student.h"
+
+typedef struct StudentManager
+{
+    ForwardList *flist;
+}StudentManager;
+
+
+
+#endif
